fd6d918a162b20163bc0ee0d3bcd342511f18465
Gabriel Lemire <gabriel.lemire@gmail.com>
2016-12-01 21:55:28 -0500

    Hello world example in RabbitMQ

---
+++ RabbitMQ/Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine($"Received '{message}'");
+                    };
+
+                    channel.BasicConsume(queue: "hello", noAck: true, consumer: consumer);
+
+                    Console.WriteLine("Client is ready and listening. Press any key to quit.");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMQ/Producer/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var shouldQuit = false;
+                    Console.WriteLine("You may now type in your message");
+
+                    while(!shouldQuit)
+                    {
+                        var message = Console.ReadLine();
+                        shouldQuit = message == "q";
+
+                        if(!shouldQuit)
+                        {
+                            var body = Encoding.UTF8.GetBytes(message);
+
+                            channel.BasicPublish(exchange: "",
+                                                 routingKey: "hello",
+                                                 basicProperties: null,
+                                                 body: body);
+
+                            Console.WriteLine($"Sent '{message}'");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
